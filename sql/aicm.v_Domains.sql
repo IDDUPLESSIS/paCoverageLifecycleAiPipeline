265ae96f8d63632d47f0d22b6fb64c1eb5b586d1
IDDUPLESSIS <86374925+IDDUPLESSIS@users.noreply.github.com>
2025-10-23 11:10:51 -0400

    Rewrite view aicm.v_Domains with new logic

--- sql/aicm.v_Domains.sql
+++ sql/aicm.v_Domains.sql
@@ -1,13 +1,156 @@
--- aicm.v_Domains
-IF OBJECT_ID('aicm.v_Domains','V') IS NOT NULL DROP VIEW aicm.v_Domains;
+USE [IBA]
 GO
-CREATE VIEW aicm.v_Domains AS
-SELECT DISTINCT
-    d.DomainClientMapId,
-    d.MasterCompanyId,
-    d.CompanyUrl,
-    d.Domain,
-    d.IsAnchor,
-    d.ConfidenceScore
-FROM aicm.DomainClientMap d;
+
+/****** Object:  View [aicm].[v_Domains]    Script Date: 10/23/2025 11:10:21 AM ******/
+SET ANSI_NULLS ON
 GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE   VIEW [aicm].[v_Domains]
+AS
+WITH S AS (
+    -- 1) Materialize only the unique keys you need from the big fact table
+    SELECT DISTINCT s.AccountName
+    FROM dbo.RAI_SP0 AS s
+    WHERE s.AccountName IS NOT NULL
+),
+J AS (
+    -- 2) Join to the lookup using a sargable equality
+    SELECT n.Domain
+    FROM MASTERDATA.dbo.MSTR_CompanyNameMapping_New AS n
+    INNER JOIN S
+        ON S.AccountName = n.ParentCompanyName
+    WHERE n.Domain IS NOT NULL
+)
+-- 3) De-dup at the end (smaller set)
+SELECT DISTINCT Domain
+FROM J;
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "sp0"
+            Begin Extent = 
+               Top = 7
+               Left = 48
+               Bottom = 170
+               Right = 421
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "namemap"
+            Begin Extent = 
+               Top = 7
+               Left = 469
+               Bottom = 170
+               Right = 743
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'aicm', @level1type=N'VIEW',@level1name=N'v_Domains'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'aicm', @level1type=N'VIEW',@level1name=N'v_Domains'
+GO
+
+
