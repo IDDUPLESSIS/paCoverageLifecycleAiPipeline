76a1df0f5fa5a5b0e1eb373f7d05b74c662c2711
IDDUPLESSIS <86374925+IDDUPLESSIS@users.noreply.github.com>
2025-10-23 11:01:08 -0400

    Recreate DomainClientMap table with new schema

--- sql/aicm.DomainClientMap.sql
+++ sql/aicm.DomainClientMap.sql
@@ -1,19 +1,32 @@
--- aicm.DomainClientMap
-IF OBJECT_ID('aicm.DomainClientMap','U') IS NOT NULL DROP TABLE aicm.DomainClientMap;
-GO
-CREATE TABLE aicm.DomainClientMap
-(
-    DomainClientMapId   INT IDENTITY(1,1) PRIMARY KEY,
-    MasterCompanyId     INT             NOT NULL,
-    CompanyUrl          NVARCHAR(255)   NOT NULL, -- e.g., https://example.com
-    Domain              NVARCHAR(255)   NOT NULL, -- e.g., example.com
-    IsAnchor            BIT             NOT NULL DEFAULT(0),
-    ConfidenceScore     DECIMAL(5,2)    NULL,
-    Source              NVARCHAR(100)   NULL,
-    CreatedOn           DATETIME2       NOT NULL DEFAULT SYSUTCDATETIME(),
-    UpdatedOn           DATETIME2       NULL
-);
+USE [IBA]
 GO
 
-CREATE UNIQUE INDEX IX_DomainClientMap_UQ ON aicm.DomainClientMap(MasterCompanyId, Domain);
+/****** Object:  Table [aicm].[DomainClientMap]    Script Date: 10/23/2025 11:00:16 AM ******/
+SET ANSI_NULLS ON
 GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [aicm].[DomainClientMap](
+	[domain_clean] [nvarchar](255) NOT NULL,
+	[client_key] [nvarchar](190) NOT NULL,
+	[source] [varchar](32) NOT NULL,
+	[created_at_utc] [datetime2](3) NOT NULL,
+	[last_seen_utc] [datetime2](3) NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [aicm].[DomainClientMap] ADD  CONSTRAINT [DF_DCKM_created]  DEFAULT (sysutcdatetime()) FOR [created_at_utc]
+GO
+
+ALTER TABLE [aicm].[DomainClientMap] ADD  CONSTRAINT [DF_DCKM_last_seen_utc]  DEFAULT (sysutcdatetime()) FOR [last_seen_utc]
+GO
+
+ALTER TABLE [aicm].[DomainClientMap]  WITH CHECK ADD  CONSTRAINT [CK_DCKM_ClientKey_NotBlank] CHECK  (([client_key] IS NOT NULL AND ltrim(rtrim([client_key]))<>''))
+GO
+
+ALTER TABLE [aicm].[DomainClientMap] CHECK CONSTRAINT [CK_DCKM_ClientKey_NotBlank]
+GO
+
+
