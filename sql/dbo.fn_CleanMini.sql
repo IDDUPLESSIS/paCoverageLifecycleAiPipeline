24432e77f6d011b4b6789c5d6da33690bee62813
IDDUPLESSIS <86374925+IDDUPLESSIS@users.noreply.github.com>
2025-10-23 11:12:39 -0400

    Revise fn_CleanMini function for better string cleaning
    Refactor fn_CleanMini to improve whitespace handling and normalization.

--- sql/dbo.fn_CleanMini.sql
+++ sql/dbo.fn_CleanMini.sql
@@ -1,15 +1,34 @@
--- dbo.fn_CleanMini (toy string cleaner)
-IF OBJECT_ID('dbo.fn_CleanMini','FN') IS NOT NULL DROP FUNCTION dbo.fn_CleanMini;
+USE [IBA]
 GO
-CREATE FUNCTION dbo.fn_CleanMini (@s NVARCHAR(MAX))
-RETURNS NVARCHAR(MAX)
+
+/****** Object:  UserDefinedFunction [dbo].[fn_CleanMini]    Script Date: 10/23/2025 11:12:21 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+/* ==============================================
+   2) Helper: strip odd whitespace & lowercase
+      (NBSP, tabs, CR/LF, NUL, BOM, zero-width)
+   ============================================== */
+CREATE   FUNCTION [dbo].[fn_CleanMini] (@s NVARCHAR(4000))
+RETURNS NVARCHAR(4000)
 AS
 BEGIN
-    IF @s IS NULL RETURN NULL;
-    DECLARE @r NVARCHAR(MAX) = LTRIM(RTRIM(@s));
-    SET @r = REPLACE(@r, CHAR(9), ' ');
-    SET @r = REPLACE(@r, CHAR(13)+CHAR(10), ' ');
-    WHILE CHARINDEX('  ', @r) > 0 SET @r = REPLACE(@r, '  ', ' ');
-    RETURN @r;
+    DECLARE @x NVARCHAR(4000) = LOWER(COALESCE(@s,''));
+    SET @x = LTRIM(RTRIM(@x));
+    SET @x = REPLACE(@x, NCHAR(160),  '');  -- NBSP
+    SET @x = REPLACE(@x, CHAR(9),     '');  -- TAB
+    SET @x = REPLACE(@x, CHAR(13),    '');  -- CR
+    SET @x = REPLACE(@x, CHAR(10),    '');  -- LF
+    SET @x = REPLACE(@x, CHAR(0),     '');  -- NUL
+    SET @x = REPLACE(@x, NCHAR(65279),'');  -- BOM
+    SET @x = REPLACE(@x, NCHAR(8203), '');  -- zero-width space
+    SET @x = REPLACE(@x, ' ',         '');  -- normal spaces
+    RETURN @x;
 END
 GO
+
+
